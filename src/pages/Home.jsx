128ab61aa6803757b8ca5a9d5fed4be021e3219d
lyra <mk.lyra.j@gmail.com>
2025-02-20 20:40:01 +0900

    🐛 Bugfix : tailwind.css 적용불가 사항 수정 @tailwind 가 unknown인 경우 PostCSS Language Support 익스텐션 설치, 그럼에도 적용이 안되는 경우 tailwind.config.js 오타 및 공백이 없는지 확인 #1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>
-      <h1>Your MBIT</h1>
+      <h1 className="bg-slate-500">Your MBIT</h1>
       <p>자신의 성격 유형을 확인할 수 있도록 솔직하게 답변해주세요.</p>
 
       <Link to="/login">로그인하기</Link>
