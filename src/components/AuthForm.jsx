774c7c31fb8593d7200ac06e14f7f0031f63ea93
lyra <mk.lyra.j@gmail.com>
2025-02-24 16:13:12 +0900

    🧹 Cleanup : 주석추가 및 로그인 변수 통일 #3

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -5,43 +5,45 @@ import { AuthContext } from "../context/AuthContext";
 import { apilogin, register } from "../api/auth";
 
 const AuthForm = ({ mode }) => {
+  // 현재 모드 확인 (true: 로그인, false: 회원가입)
   const isLoginMode = mode === "login";
   const navigate = useNavigate();
+
+  // AuthContext에서
   const { loginUser } = useContext(AuthContext);
 
+  // 입력 폼 상태관리 커스텀 훅
   const { formData, handleChange, resetForm } = useForm({
     id: "",
     password: "",
     nickname: "",
   });
 
-  // Login login
+  // 로그인 함수
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      // NOTE: login
+      // 로그인 API 호출
       const data = await apilogin({
         id: formData.id,
         password: formData.password,
       });
 
       alert("로그인 완료");
-      // 로그인 상태를 브라우저에 저장
-      loginUser(data.accessToken);
-      navigate("/profile");
+      loginUser(data.accessToken); // 로그인 상태를 브라우저에 저장
+      navigate("/profile"); // 프로필 페이지로 이동
     } catch (error) {
-      console.error("Login error", error);
+      console.error("⛔️Login error", error);
       alert("로그인에 실패했습니다.");
-      // alert(error.response.data.message);
     }
   };
 
-  // Signup logic
+  // 회원가입 함수
   const handleSignup = async (e) => {
     e.preventDefault();
 
     try {
-      // NOTE: signup
+      // 회원가입 API 호출
       await register({
         id: formData.id,
         password: formData.password,
@@ -54,14 +56,14 @@ const AuthForm = ({ mode }) => {
     } catch (error) {
       console.error("⛔️Signup error", error);
       alert("회원가입에 실패했습니다.");
-      //   alert(`오류 캐치 : ${error.response.data.message} || "네트워크 오류"`);
     }
   };
 
   return (
     <div>
       <form
-        onSubmit={mode === "login" ? handleLogin : handleSignup}
+        // 로그인 모드에 따라 핸들러 함수 실행
+        onSubmit={isLoginMode ? handleLogin : handleSignup}
         className="bg-gray-50 p-6 rounded-lg shadow-md space-y-6"
       >
         <input
@@ -83,7 +85,7 @@ const AuthForm = ({ mode }) => {
           className="w-full p-4 border border-gray-200 rounded-lg"
         />
 
-        {/* 회원가입시 보이는 닉네임 input */}
+        {/* 회원가입시에만 보이는 닉네임 input */}
         {!isLoginMode && (
           <input
             type="text"
@@ -99,6 +101,7 @@ const AuthForm = ({ mode }) => {
           type="submit"
           className="w-full bg-violet-500 text-white py-3 rounded-lg hover:bg-violet-800 transition duration-300"
         >
+          {/* 로그인 모드에 따라 보이는 로그인/회원가입 버튼 */}
           {isLoginMode ? "로그인" : "회원가입"}
         </button>
       </form>
