d9573ccd3cf662a5ce95529cfbc9a694df2525fa
lyra <mk.lyra.j@gmail.com>
2025-02-24 09:42:05 +0900

    ✨ Update : Home페이지 로그인여부에 따른 페이지 설정 추가 #3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,38 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 const Home = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleTestNavigation = () => {
+    const confirmLogin = window.confirm(
+      "로그인 후 이용가능합니다. 로그인페이지로 이동하시겠습니까?"
+    );
+    if (isAuthenticated) {
+      return navigate("/test");
+    }
+
+    if (!isAuthenticated) {
+      if (confirmLogin) {
+        navigate("/login");
+      } else {
+        navigate("/");
+      }
+    }
+  };
+
   return (
-    <div className="w-full flex flex-col items-center justify-center bg-gray-100">
+    <div className="w-full flex flex-col items-center justify-center bg-gray-50">
       <div>
-        <h1 className="text-5xl font-bold mb-6">Your MBTI</h1>
+        {/* <h1 className="text-5xl font-bold mb-6">Your MBTI</h1> */}
         <h2 className="text-3xl font-semibold mb-5">무료 성격 테스트</h2>
         <p className="text-lg mb-8">
           자신의 성격 유형을 확인할 수 있도록 솔직하게 답변해주세요.
         </p>
       </div>
-      <ul className="flex gap-8">
+      <ul className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-12">
         <li className="bg-white p-5 rounded-lg shadow-lg">
           <h3 className="text-xl font-semibold mb-4">성격 유형 검사</h3>
           <p className="text-gray-600">
@@ -33,8 +54,13 @@ const Home = () => {
           </p>
         </li>
       </ul>
-      <Link to="/login">로그인하기</Link>
-      <Link to="/test">내 성격 알아보러 가기</Link>
+
+      <button
+        onClick={handleTestNavigation}
+        className="inline-block bg-violet-500 text-white py-2 px-6 rounded-full hover:bg-secondary-color transition mb-4 hover:bg-violet-800"
+      >
+        내 성격 알아보러 가기
+      </button>
     </div>
   );
 };
