b200ff1fc98319cc70aecedad5a8db4ffa45d542
lyra <mk.lyra.j@gmail.com>
2025-02-25 14:03:59 +0900

    🌐 Chore : react toastify 추가 및 셋팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Router from "./shared/Router";
 import AuthProvider from "./context/AuthContext";
+import { Bounce, ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <AuthProvider>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
       <Router />
     </AuthProvider>
   );
